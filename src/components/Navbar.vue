582f639805df89f67f3cb637a6fc65decb26cbf1
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-08-24 23:09:04 +0200

    div refactoring

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex justify-between">
-    <a href="/" class="hidden sm:block sm:fixed top-2">Gamle Oslo Kaffe og Koding</a>
-    <div class="hidden sm:block"></div>
+    <a href="/" class="hidden md:block md:fixed top-2">Gamle Oslo Kaffe og Koding</a>
+    <div class="hidden md:block"></div>
     <ul class="flex justify-end gap-2 mt-1">
       <li><a href="/">Hjem</a></li>
       <li><a href="/about">Om oss</a></li>
       <li><a href="/join">Bli medlem</a></li>
+      <li><a href="/contact">Kontakt</a></li>
     </ul>
   </div>
 </template>
