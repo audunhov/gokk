473c0c2fbc88c691a9cb6ec9b43e0edf001dbac6
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-08-24 22:39:26 +0200

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <ul class="absolute bottom-2">
+        <li>Org.nr: 829 662 272</li>
+    </ul>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between">
+    <a href="/" class="hidden sm:block sm:fixed top-2">Gamle Oslo Kaffe og Koding</a>
+    <div class="hidden sm:block"></div>
+    <ul class="flex justify-end gap-2 mt-1">
+      <li><a href="/">Hjem</a></li>
+      <li><a href="/about">Om oss</a></li>
+      <li><a href="/join">Bli medlem</a></li>
+    </ul>
+  </div>
+</template>
