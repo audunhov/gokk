2f5b02b80c0859bdcd47a94a678495bb14bc8c24
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-08-25 23:19:53 +0200

    fiksa routing når base er satt

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex justify-between fixed right-2">
     <ul class="flex items-end gap-2 mt-1">
-      <li><a href="/">Hjem</a></li>
-      <li><a href="/about">Om oss</a></li>
-      <li><a href="/join">Bli medlem</a></li>
-      <li><a href="/contact">Kontakt</a></li>
+      <li><a href=".">Hjem</a></li>
+      <li><a href="about">Om oss</a></li>
+      <li><a href="join">Bli medlem</a></li>
+      <li><a href="contact">Kontakt</a></li>
     </ul>
   </div>
 </template>
