8133bab11da76a96fb91f16e077e9e56ca99b518
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-09-01 23:31:45 +0200

    fiksa linker

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <div class="flex justify-between fixed right-2">
     <ul class="flex items-end gap-2 mt-1">
       <li><a href="/gokk/">Hjem</a></li>
-      <li><a href="/gokk/about">Om oss</a></li>
-      <li><a href="/gokk/join">Bli medlem</a></li>
-      <li><a href="/gokk/contact">Kontakt</a></li>
+      <li><a href="/gokk/about/">Om oss</a></li>
+      <li><a href="/gokk/join/">Bli medlem</a></li>
+      <li><a href="/gokk/contact/">Kontakt</a></li>
     </ul>
   </div>
 </template>
