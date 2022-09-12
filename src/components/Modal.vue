c2043184fe124d3c4c880a6d40aa8ca634089264
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-09-12 23:12:22 +0200

    gjorde om navigation til hamburger på små skjermer

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="isOpen" >
+      <div
+          class="
+          fixed h-full w-full
+          top-0 z-10 left-0 m-0
+          flex justify-center items-center
+          bg-black bg-opacity-80
+          transition-all"
+          @click="emit('close')">
+        <slot/>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineEmits} from "vue";
+
+defineProps<{
+  isOpen: {
+    type: Boolean,
+    default: false
+  }
+}>()
+
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="flex justify-between fixed right-2">
-    <ul class="flex items-end gap-2 mt-1">
-      <li><a href="/gokk/">Hjem</a></li>
-      <li><a href="/gokk/about/">Om oss</a></li>
-      <li><a href="/gokk/join/">Bli medlem</a></li>
-      <li><a href="/gokk/contact/">Kontakt</a></li>
-    </ul>
+  <div class="flex justify-end">
+    <div class="block sm:hidden">
+      <button @click="open = true" class="text-4xl">=</button>
+      <Modal :isOpen="open" @close="open = false">
+        <ul class="space-y-4 text-center w-full">
+          <li><a href="/gokk/" class="w-1/2 block m-auto">Hjem</a></li>
+          <li><a href="/gokk/about/" class="w-1/2 block m-auto">Om oss</a></li>
+          <li><a href="/gokk/join/" class="w-1/2 block m-auto">Bli medlem</a></li>
+          <li><a href="/gokk/contact/" class="w-1/2 block m-auto">Kontakt</a></li>
+        </ul>
+      </Modal>
+    </div>
+
+    <div class="hidden sm:block">
+      <ul class="space-x-2 text-center flex justify-baseline">
+        <li><a href="/gokk/">Hjem</a></li>
+        <li><a href="/gokk/about/">Om oss</a></li>
+        <li><a href="/gokk/join/">Bli medlem</a></li>
+        <li><a href="/gokk/contact/">Kontakt</a></li>
+      </ul>
+    </div>
   </div>
+
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import Modal from "./Modal.vue"
+const open = ref(false)
+</script>
